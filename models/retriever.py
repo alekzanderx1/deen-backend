a7df232e65050f932265404ad462ac472bc26141
Shawn Nassabi <san7522@nyu.edu>
2025-04-03 13:59:48 +0400

    Reference lookup api now returns both shia and sunni content

--- models/retriever.py
+++ models/retriever.py
@@ -1,5 +1,5 @@
 from pinecone import Pinecone, ServerlessSpec
-from config import PINECONE_API_KEY, DEEN_INDEX_LINK
+from config import PINECONE_API_KEY, DEEN_INDEX_LINK, DEEN_SUNNI_INDEX_LINK
 
 # Initialize a client
 pc = Pinecone(api_key=PINECONE_API_KEY)
@@ -8,6 +8,36 @@ pc = Pinecone(api_key=PINECONE_API_KEY)
 index = pc.Index(host=DEEN_INDEX_LINK)
 
 def retrieve_documents(query_embedding):
+    # Connect to your index
+    index = pc.Index(host=DEEN_INDEX_LINK)
+    print("INSIDE retrieve_documents")
+    results = index.query(
+        namespace="ns1",
+        vector=query_embedding,
+        top_k=7,
+        include_values=False,
+        include_metadata=True
+    )
+    print("Documents retrieved. Example:", results["matches"][0]["metadata"])
+    return [match["metadata"] for match in results["matches"]]
+
+def retrieve_shia_documents(query_embedding):
+    # Connect to your index
+    index = pc.Index(host=DEEN_INDEX_LINK)
+    print("INSIDE retrieve_documents")
+    results = index.query(
+        namespace="ns1",
+        vector=query_embedding,
+        top_k=7,
+        include_values=False,
+        include_metadata=True
+    )
+    print("Documents retrieved. Example:", results["matches"][0]["metadata"])
+    return [match["metadata"] for match in results["matches"]]
+
+def retrieve_sunni_documents(query_embedding):
+    # Connect to your index
+    index = pc.Index(host=DEEN_SUNNI_INDEX_LINK)
     print("INSIDE retrieve_documents")
     results = index.query(
         namespace="ns1",
