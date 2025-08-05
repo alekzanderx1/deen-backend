f0dbc76ab897d260b362a216882e145b10ad8120
Ahmad <ahmadtaqi789@gmail.com>
2025-08-05 10:36:29 -0400

    Changes to integrate sparse and dense vector search for reference retrieval in the chat pipeline.

--- core/config.py
+++ core/config.py
@@ -7,9 +7,16 @@ load_dotenv()
 # Retrieve API Keys
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-DEEN_INDEX_NAME = os.getenv("DEEN_INDEX_LINK")
-DEEN_SUNNI_INDEX_NAME = os.getenv("DEEN_SUNNI_INDEX_LINK")
+DEEN_DENSE_INDEX_NAME = os.getenv("DEEN_DENSE_INDEX_NAME")
+DEEN_SPARSE_INDEX_NAME = os.getenv("DEEN_SPARSE_INDEX_NAME")
+DENSE_RESULT_WEIGHT = os.getenv("DENSE_RESULT_WEIGHT",0.7)
+SPARSE_RESULT_WEIGHT = os.getenv("SPARSE_RESULT_WEIGHT",0.3)
+REFERENCE_FETCH_COUNT = int(os.getenv("REFERENCE_FETCH_COUNT", 10))
 
 # Check if keys are loaded (optional)
 if not OPENAI_API_KEY or not PINECONE_API_KEY:
     raise ValueError("Missing API keys! Ensure they are set in the .env file.")
+
+# Check if keys are loaded (optional)
+if not DEEN_DENSE_INDEX_NAME or not DEEN_SPARSE_INDEX_NAME:
+    raise ValueError("Missing Index names! Ensure they are set in the .env file.")
---
+++ core/constants.py
@@ -0,0 +1,24 @@
+# Normalize Islamic terminology
+ISLAMIC_TERMS_MAP = {
+    "salat": "salah",
+    "salaah": "salah",
+    "salaat": "salah",
+    "zakaat": "zakat",
+    "zakah": "zakat",
+    "dhikr": "zikr",
+    "dhikrullah": "zikr",
+    "sawm": "fasting",
+    "ramadhan": "ramadan",
+    "hadeeth": "hadith",
+    "sahabah": "companion",
+    "sahabi": "companion",
+    "sahaba": "companion",
+    "koran": "quran",
+    "hussein": "hussain",
+    "hossein": "hussain",
+    "mohamad": "muhammad",
+    "muhamad": "muhammad",
+    "mohamed": "muhammad",
+    "wudhu": "wudu",
+    "wuzu": "wudu"
+}
